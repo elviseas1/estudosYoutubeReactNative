58944b82ea7b6b1ac05e0ec12f3ba0bc3e55e839
Elvis Silveira <elvis@Perbrass-MacBook-Pro.local>
2019-11-04 21:14:15 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React, {Component} from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default class App extends Component {
+
+  render(){
+  return (
+    <View>
+      <Text>
+        Vamos iniciar no React Native
+      </Text>
+      <Text>
+        Para iniciar edit p arquivo app.js
+      </Text>
+    </View>
+  )
+  }
+}
