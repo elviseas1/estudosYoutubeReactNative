db637a973f2d382adb5bdf71e72bba8da8c57cae
Elvis Silveira <elvis@Perbrass-MacBook-Pro.local>
2019-11-16 20:38:19 -0300

    class6_1

--- App.js
+++ App.js
@@ -7,13 +7,13 @@ import {
   Image
 } from 'react-native';
 
-export default class FixedDimensionsApp extends Component{
+export default class FlexDimensionsApp extends Component{
   render(){
     return(
-      <View style={{padding: 35}}>
-        <View style={{width:100, height: 100, backgroundColor: 'powderblue'}}></View>
-        <View style={{width:150, height: 150, backgroundColor: 'skyblue'}}></View>
-        <View style={{width:300, height: 300, backgroundColor: 'steelblue'}}></View>
+      <View style={{height: 400}}>
+        <View style={{flex:1, backgroundColor: 'powderblue'}}></View>
+        <View style={{flex:2, backgroundColor: 'skyblue'}}></View>
+        <View style={{flex:3, backgroundColor: 'steelblue'}}></View>
       </View>
     );
   }
