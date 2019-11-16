d8cd195f10eff7bd28efe67aca2796f9351c6a58
Elvis Silveira <elvis@Perbrass-MacBook-Pro.local>
2019-11-16 20:31:04 -0300

    class6

--- App.js
+++ App.js
@@ -7,26 +7,13 @@ import {
   Image
 } from 'react-native';
 
-const styles = StyleSheet.create({
-  bigblue:{
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 50, 
-  },
-  red: {
-    color: 'red',
-    fontSize: 30, 
-  }
-})
-
-export default class StylesApp extends Component{
+export default class FixedDimensionsApp extends Component{
   render(){
     return(
-      <View style={{padding: 50}}>
-        <Text style={styles.red}>Vermelho</Text>
-        <Text style={styles.bigblue}>Azul</Text>
-        <Text style={[styles.bigblue, styles.red]}>Azul, depois Vermelho</Text>
-        <Text style={[styles.red, styles.bigblue]}>Vermelho, depois Azul</Text>
+      <View style={{padding: 35}}>
+        <View style={{width:100, height: 100, backgroundColor: 'powderblue'}}></View>
+        <View style={{width:150, height: 150, backgroundColor: 'skyblue'}}></View>
+        <View style={{width:300, height: 300, backgroundColor: 'steelblue'}}></View>
       </View>
     );
   }
