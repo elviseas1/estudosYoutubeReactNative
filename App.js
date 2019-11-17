f6917dd7e7bd7043c923bfd2ae36e73a8e4d931a
Elvis Silveira <elvis@Perbrass-MacBook-Pro.local>
2019-11-17 08:13:57 -0300

    class9

--- App.js
+++ App.js
@@ -5,27 +5,61 @@ import {
   Text,
   StyleSheet,
   Image,
-  TextInput
+  TextInput,
+  Button,
+  Alert,
 } from 'react-native';
 
-export default class PizzaTranslatorApp extends Component{
-  constructor(props){
-    super(props);
-    this.state = {text: ''};
-  };
+export default class ButtonApp extends Component{
+  onPressButton(){
+    Alert.alert('Você clicou no botão!')
+  }
 
   render(){
     return(
-      <View style={{padding: 40}}>
-        <TextInput 
-          style={{height: 50, fontSize: 30}}
-          placeholder= 'Digite aqui para traduzir!'
-          onChangeText= {(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 50}}>
-          {this.state.text.split(' ').map( (word) => (word) ? ':P' : ' ' )}
-        </Text>
-      </View> 
+      <View style={styles.container}>
+        <View style={styles.butonContainer}>
+          <Button 
+            onPress= {this.onPressButton}
+            title= 'Clique aqui!'
+          />
+        </View>
+        <View style={styles.butonContainer}>
+          <Button 
+            onPress= {this.onPressButton}
+            title= 'Clique aqui!'
+            color= '#841584'
+          />
+        </View> 
+        <View style={styles.alternativeButtonContainer}>
+          <Button 
+          onPress= {this.onPressButton}
+          title= 'Parece ok!'
+          />
+        </View>  
+        <View style={styles.alternativeButtonContainer}>
+          <Button 
+          onPress= {this.onPressButton}
+          title= 'ok!'
+          color= '#841584'
+          />
+        </View> 
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+     flex: 1,
+    justifyContent: 'center',
+  },
+  butonContainer: {
+    margin: 20,
+  },
+  alternativeButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
